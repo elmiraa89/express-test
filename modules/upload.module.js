3288c976b996e06e688750ab7efcee6de7b7e511
elmiraa89 <liaelmira89@gmail.com>
2025-07-14 15:20:49 +0700

    Update upload.module.js

--- modules/upload.module.js
+++ modules/upload.module.js
@@ -5,7 +5,7 @@ import path from 'path';
 
 const s3 = new AWS.S3({
     region: process.env.AWS_REGION,
-    accessKeyId: pprocess.env.AWS_ACCESS_KEY_ID,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 })
 const fileFilter = (req, file, cb) => {
